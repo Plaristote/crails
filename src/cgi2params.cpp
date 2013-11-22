152c7a2bc2b5529e2a271702543909081e534466
Michael Martin Moro <sexymimi64@gmail.com>
2013-11-22 19:54:13 +0000

    Fixed parsing issue in cgi2params

--- src/cgi2params.cpp
+++ src/cgi2params.cpp
@@ -8,10 +8,10 @@ using namespace std;
 static void RecursivelySetValue(Data param, std::vector<std::string> key_stack, const std::string& value)
 {
   if (key_stack.size() == 0)
-    param = value;
+    param = Http::Url::Decode(value);
   else
   {
-    Data next = param[key_stack.front()];
+    Data next = param[Http::Url::Decode(key_stack.front())];
 
     key_stack.erase(key_stack.begin());
     RecursivelySetValue(next, key_stack, value);
@@ -20,7 +20,9 @@ static void RecursivelySetValue(Data param, std::vector<std::string> key_stack, 
 
 void cgi2params(DynStruct& params, const std::string& encoded_str)
 {
-  std::string    str        = Http::Url::Decode(encoded_str);  
+  const std::string opening_bracket = "%5B";
+  const std::string closing_bracket = "%5D";
+  std::string    str        = encoded_str;//        = Http::Url::Decode(encoded_str);  
   std::string    looping    = "";
   std::vector<std::string> key_stacks;
 
@@ -35,21 +37,21 @@ void cgi2params(DynStruct& params, const std::string& encoded_str)
     regex.SetPattern("[^=&]*", REG_EXTENDED);
     if (!(regex.Match(str, &match, 1)))
     {
-      auto sub_key = str.find('[');
+      auto sub_key = str.find(opening_bracket);
 
       if (sub_key != std::string::npos && (int)sub_key < match.rm_eo)
         match.rm_eo = sub_key;
       key_stacks.push_back(str.substr(match.rm_so, match.rm_eo));
       str.erase(match.rm_so, match.rm_eo);
 
-      while (str[0] == '[')
+      while (str.find(opening_bracket) == 0)
       {
-        str.erase(0, 1);
-        auto end_key = str.find(']');
+        str.erase(0, opening_bracket.size());
+        auto end_key = str.find(closing_bracket);
         if (end_key != std::string::npos)
         {
           key_stacks.push_back(str.substr(0, end_key));
-          str.erase(0, end_key + 1);
+          str.erase(0, end_key + closing_bracket.size());
         }
         else
           return ;
