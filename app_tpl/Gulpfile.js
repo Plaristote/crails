be9e24a6b21acd01c4c8e08576f4a845b93a5fda
Michael Martin Moro <michael@unetresgrossebite.com>
2014-11-03 16:09:03 +0100

    * Gulpfile now compiles database plugins (/lib/db) * Gulpfile can now compile dynamic libraries for both Linux and OSX

--- app_tpl/Gulpfile.js
+++ app_tpl/Gulpfile.js
@@ -1,3 +1,4 @@
+var os              = require('os');
 var gulp            = require('gulp');
 var coffee          = require('gulp-coffee');
 var concat          = require('gulp-concat');
@@ -9,14 +10,39 @@ var child           = require('child_process');
 var require_nocache = require('require-without-cache');
 var assets          = require_nocache('./config/assets.json', require);
 
-var debug  = true;
+var debug            = true;
+var dynlib_extension = 'so';
+
+switch (os.platform())
+{
+  case 'darwin':
+    dynlib_extension = 'dylib';
+    break ;
+  case 'linux':
+    dynlib_extension = 'so';
+    break ;
+  case 'win32':
+    dynlib_extension = 'dll';
+    break ;
+}
+
+function command_compile(command)
+{
+  var compiler    = 'c++';
+  var opts        = [ '-fPIC', '--std=c++11', '-shared', '-Wall', '-Wno-return-type-c-linkage', '-Wno-unused-private-field' ];
+  if (debug)                     { opts.push('-g'); }
+  if (os.platform() == 'darwin') { opts.push('-undefined dynamic_lookup'); }
+
+  return (compiler + ' ' + opts.join(' ') + ' ' + command);
+}
 
 var paths = {
   cmake:   [ 'build/CMakeCache.txt' ],
   scripts: [ 'app/assets/javascripts/**/*.js', 'app/assets/javascripts/**/*.coffee' ],
   css:     [ 'app/assets/stylesheets/**/*.scss', 'app/assets/stylesheets/**/*.css' ],
   views:   [ 'app/views/**/*.ecpp', 'lib/*.ecpp' ],
-  models:  [ 'app/models/**/*.hpp' ]
+  models:  [ 'app/models/**/*.hpp' ],
+  plugins: [ 'lib/db/*.cpp' ]
 }
 
 gulp.task('cmake', function() {
@@ -64,12 +90,13 @@ gulp.task('assets.json', ['clean'], function() {
 });
 
 gulp.task('views', ['clean'], function() {
-  var compiler    = 'c++';
-  var opts        = [ '-Wall', '-fPIC', '--std=c++11', '-shared' ]
-  if (debug) { opts.push('-g'); }
   var compile_erb = gulp.src(paths.views).pipe(exec('crails compile_view <%= file.path %>'), { continueOnError: false, pipeStdout: false }).pipe(exec.reporter({ err: true, stderr: true, stdout: true }));
-  var compile_cpp = compile_erb.pipe(exec(compiler + ' ' + opts.join(' ') + ' -I./app <%= file.path %>.cpp -o <%= file.path %>.so'), { continueOnError: false, pipeStdout: false }).pipe(exec.reporter({ err: true, stderr: true, stdout: true }));
+  var compile_cpp = compile_erb.pipe(exec(command_compile('-I./app <%= file.path %>.cpp -o <%= file.path %>.' + dynlib_extension)), { continueOnError: false, pipeStdout: false }).pipe(exec.reporter({ err: true, stderr: true, stdout: true }));
   return (compile_cpp);
+});
+
+gulp.task('plugins', ['clean'], function() {
+  return (gulp.src(paths.plugins).pipe(exec(command_compile('<%= file.path %>.cpp -o <%= file.path %>.' + dynlib_extension)), { continueOnError: false, pipeStdout: false }).pipe(exec.reporter({ err: true, stderr: true, stdout: true })));
 });
 
 gulp.task('models', ['clean'], function() {
@@ -87,6 +114,7 @@ gulp.task('watch', function() {
   gulp.watch(paths.css,     ['css']);
   gulp.watch(paths.views,   ['views']);
   gulp.watch(paths.models,  ['models']);
+  gulp.watch(paths.plugins, ['plugins']);
 });
 
-gulp.task('default', ['watch', 'cmake', 'scripts', 'css', 'views', 'models']);
+gulp.task('default', ['watch', 'cmake', 'scripts', 'css', 'views', 'models', 'plugins']);
