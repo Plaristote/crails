d71140fb240d552d7ef2768d8d1801a7fe816ab7
Michael Martin Moro <michael@unetresgrossebite.com>
2015-10-08 08:33:02 +0200

    fixed mongodb module including the wrong cookie store

---
+++ modules/mongodb/crails/session_store/mongodb.hpp
@@ -0,0 +1,49 @@
+#ifndef  MONGO_STORE_HPP
+# define MONGO_STORE_HPP
+
+# include "crails/cookie_data.hpp"
+# include "crails/http_response.hpp"
+# include "crails/session_store.hpp"
+
+# include <crails/mongodb/model.hpp>
+
+namespace Crails
+{
+  class MongoStore : public SessionStore
+  {
+  public:
+    class SessionStore : public MongoDB::Model
+    {
+    public:
+      static const std::string& CollectionName(void) { return (collection_name); }
+      static const std::string& DatabaseName(void)   { return (database_name);   }
+
+      MONGODB_MODEL(SessionStore)
+
+      void set_fields(Data data);
+      void get_fields(Data data);
+      void save(void);
+
+      static void Cleanup(void);
+
+      static const std::string  collection_name;
+      static const std::string  database_name;
+      static const unsigned int session_expiration;
+    };
+
+    MongoStore();
+    ~MongoStore();
+
+    void             load(Data request_headers);
+    void             finalize(BuildingResponse& response);
+    DynStruct&       to_data(void)       { return (session_content); }
+    const DynStruct& to_data(void) const { return (session_content); }
+    
+  private:
+    SmartPointer<SessionStore> session;
+    CookieData                 cookie;
+    DynStruct                  session_content;
+  };
+}
+
+#endif
