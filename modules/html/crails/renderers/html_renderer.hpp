5c39a800fb16a6aa449481497315e80bf475635a
Michael Martin Moro <michael@unetresgrossebite.com>
2015-10-18 19:19:16 +0200

    moves HtmlRenderer::render_template to a source file

--- modules/html/crails/renderers/html_renderer.hpp
+++ modules/html/crails/renderers/html_renderer.hpp
@@ -11,20 +11,7 @@ namespace Crails
     HtmlRenderer();
 
     bool can_render(const std::string& accept_header, const std::string& view) const;
-    void render_template(const std::string& view, Data params, Data response, SharedVars& vars) const
-    {
-      auto tpl  = templates.find(view);
-      std::string html_view = (*tpl).second((vars));
-
-      response["headers"]["Content-Type"] = "text/html";
-      if (response["layout"].NotNil() && view != response["layout"].Value())
-      {
-	vars["yield"] = &html_view;
-	render(response["layout"], params, response, vars);
-      }
-      else
-	response["body"] = html_view;
-    }    
+    void render_template(const std::string& view, Data params, Data response, SharedVars& vars) const;
   };
 }
 
