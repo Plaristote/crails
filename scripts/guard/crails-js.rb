1e24b38391956fa46861c26d8985f57724f3c048
Michael Martin Moro <michael@unetresgrossebite.com>
2015-11-14 21:09:02 +0100

    implements require_tree in crails_js also fixes the lack of "#= require" in CoffeeScript

--- scripts/guard/crails-js.rb
+++ scripts/guard/crails-js.rb
@@ -1,5 +1,6 @@
 require 'coffee-script'
 require 'fileutils'
+require 'pathname'
 
 module ::Guard
   class CrailsJs < Plugin
@@ -9,6 +10,7 @@ module ::Guard
     end
 
     def run_all
+      @included_files = []
       options[:targets].each do |target|
         text        = compile_file target
         target      = extension_to_js target
@@ -31,19 +33,36 @@ module ::Guard
     end
 
     def compile_file file
+      @included_files << file
       current_dir       = file.split('/')[0...-1].join('/')
       full_path         = (options[:input] + '/' + file).gsub(/\/+/, '/')
       source            = File.read full_path, encoding: 'BINARY'
-      source            = CoffeeScript.compile source, bare: true if file.match(/\.coffee$/) != nil
+      if file.match(/\.coffee$/) != nil
+        source          = source.gsub /^#=([^\n]+)/, '`//=\1`'
+        source          = CoffeeScript.compile source, bare: true
+      end
       comment_character = "//"
       file_content      = ''
       source.split("\n").each do |line|
-        matches = line.match(/^\s*#{comment_character}=\s+(require|include)\s+([^;]+)/)
+        matches = line.match(/^\s*#{comment_character}=\s+(require|require_tree|include)\s+([^;]+)/)
         if matches.nil?
           file_content += line + "\n"
         else
-          required_filename = current_dir + '/' + matches[2]
-          file_content     += (compile_file required_filename.strip) + "\n"
+          required_path = Pathname.new(current_dir + '/' + matches[2].strip).cleanpath.to_s
+          puts "Required path: #{required_path}"
+          if matches[1] == 'require_tree'
+            path = "#{options[:input]}/#{required_path}/**/*"
+            files = Dir.glob("#{path}.coffee") + Dir.glob("#{path}.js")
+            puts files.inspect
+            files.each do |filepath|
+              filepath = filepath[options[:input].size..-1]
+              unless @included_files.include? filepath
+                file_content += (compile_file filepath) + "\n"
+              end
+            end
+          elsif not @included_files.include? required_path
+            file_content     += (compile_file required_path) + "\n"
+          end
         end
       end
       file_content
