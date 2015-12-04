de6cfd41e08dd1025d92dcda38129446bdc31773
Michael Martin Moro <michael@unetresgrossebite.com>
2015-12-04 11:32:02 +0100

    crailsjs now properly reports coffeescript compilation errors

--- scripts/guard/crails-js.rb
+++ scripts/guard/crails-js.rb
@@ -13,18 +13,23 @@ module ::Guard
     end
 
     def run_all
-      @included_files = []
-      options[:targets].each do |target|
-        text        = compile_file target
-        target      = extension_to_js target
-        output_file = options[:output] + '/' + target
-        FileUtils.mkdir_p options[:output]
-        File.open(output_file, 'w') do |f|
-          js = "(function() {\n#{text}\n})();".force_encoding('UTF-8')
-          js = Uglifier.compile(js, @uglifier_options) unless developer_mode?
-          f.write js
+      begin
+        @included_files = []
+        options[:targets].each do |target|
+          text        = compile_file target
+          target      = extension_to_js target
+          output_file = options[:output] + '/' + target
+          FileUtils.mkdir_p options[:output]
+          File.open(output_file, 'w') do |f|
+            js = "(function() {\n#{text}\n})();".force_encoding('UTF-8')
+            js = Uglifier.compile(js, @uglifier_options) unless developer_mode?
+            f.write js
+          end
+          puts ">> Generated #{output_file}"
         end
-        puts ">> Generated #{output_file}"
+      rescue Exception => e
+        puts_error "Catched exception: #{e.message}"
+        puts_error "!! crailsjs couldn't perform its duties"
       end
     end
 
@@ -41,6 +46,10 @@ module ::Guard
       Pathname.new(filepath).cleanpath.to_s
     end
 
+    def puts_error str
+      puts "\033[31m#{str}\033[0m"
+    end
+
     def compile_file file
       @included_files << file
       current_dir       = file.split('/')[0...-1].join('/')
@@ -48,7 +57,12 @@ module ::Guard
       source            = File.read full_path, encoding: 'BINARY'
       if file.match(/\.coffee$/) != nil
         source          = source.gsub /^#=([^\n]+)/, '`//=\1`'
-        source          = CoffeeScript.compile source, bare: true
+        begin
+          source        = CoffeeScript.compile source, bare: true
+        rescue ExecJS::RuntimeError => e
+          puts_error "CoffeeScript compilation failed for `#{file}`: #{e.message}"
+          raise "compilation error"
+        end
       end
       comment_character = "//"
       file_content      = ''
