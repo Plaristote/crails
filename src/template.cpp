0815e76a4e4e7764cc48ee89610d569e5cefd880
Michael Martin Moro <michael@unetresgrossebite.com>
2016-02-19 13:52:26 +0100

    fix shared vars in partial not being properly replicated

--- src/template.cpp
+++ src/template.cpp
@@ -3,7 +3,7 @@
 using namespace Crails;
 using namespace std;
 
-std::string Template::partial(const std::string& view, SharedVars vars)
+std::string Template::partial(const std::string& view, SharedVars vars_)
 {
   auto tpl = renderer->templates.find(view);
 
@@ -13,7 +13,7 @@ std::string Template::partial(const std::string& view, SharedVars vars)
   {
     SharedVars duplicate = vars;
 
-    for (auto& var : vars)
+    for (auto& var : vars_)
       duplicate[var.first] = var.second;
     return (*tpl).second(renderer, duplicate);
   }
