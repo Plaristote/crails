889d0f1a136c702493b63576d0496db1627793ed
Michael Martin Moro <michael@unetresgrossebite.com>
2017-01-31 11:20:49 +0100

    fixed request parser not working with different cases of the Content-Type header

--- src/request_parser.cpp
+++ src/request_parser.cpp
@@ -6,11 +6,17 @@ using namespace Crails;
 
 bool RequestParser::content_type_matches(Params& params, const regex regexp)
 {
-  if (params["headers"]["Content-Type"].exists())
-  {
-    string type = params["headers"]["Content-Type"].as<string>();
+  Data           headers = params["headers"];
+  vector<string> supported_headers = {"Content-Type","content-type"};
 
-    return regex_search(type, regexp);
+  for (string header_key : supported_headers)
+  {
+    if (params["headers"][header_key].exists())
+    {
+      string type = params["headers"][header_key].as<string>();
+
+      return regex_search(type, regexp);
+    }
   }
   return false;
 }
