625d0cbca030c9e7752c2197226ee6445f3bd699
Michael <michael@unetresgrossebite.com>
2018-05-29 14:21:10 +0200

    fixed missing namespace in app_template's config/ssl.cpp

--- app_template/app/config/ssl.cpp
+++ app_template/app/config/ssl.cpp
@@ -1,7 +1,10 @@
 #include <boost/asio.hpp>
 #include <boost/asio/ssl.hpp>
 
-namespace Crails {
+using namespace boost;
+
+namespace Crails
+{
   std::string get_ssl_password(std::size_t max_length, asio::ssl::context_base::password_purpose purpose)
   {
     return "your_ssl_password";
