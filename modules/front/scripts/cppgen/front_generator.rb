b0ed6f25d29f0253140da3309a0b53785f1a2b60
Michael <michael@unetresgrossebite.com>
2018-11-28 19:17:28 +0100

    crails-front model generator now implements to_json/from_json methods

--- modules/front/scripts/cppgen/front_generator.rb
+++ modules/front/scripts/cppgen/front_generator.rb
@@ -28,6 +28,7 @@ class FrontGenerator < GeneratorBase
     _append "# define #{defname}"
     _append ""
     _append "# include <crails/front/mvc/model.hpp>"
+    _append "# include <crails/datatree.hpp>"
     _append ""
     generate_class object
     _append ""
@@ -45,6 +46,7 @@ class FrontGenerator < GeneratorBase
     self.instance_eval &object[:block]
     @src += "\n"
     generate_serializers object
+    generate_jsonifiers object
     @indent -= 1
     _append "protected:"
     @indent += 1
@@ -74,6 +76,23 @@ class FrontGenerator < GeneratorBase
     _append "}\n"
   end
 
+  def generate_jsonifiers object
+    @state = :to_json
+    _append "std::string to_json() const"
+    _append "{"
+    @indent += 1
+    _append "DataTree data;\n"
+    self.instance_eval &object[:block]
+    _append "return data.to_json();"
+    @indent -= 1
+    _append "}\n"
+    @state = :from_json
+    _append "void from_json(Data data)"
+    _append "{"
+    self.instance_eval &object[:block]
+    _append "}\n"
+  end
+
   def property type, name, options = {}
     return if not options[:client].nil? and options[:client][:ignore] == true
     if @state == :field_scope
@@ -87,6 +106,15 @@ class FrontGenerator < GeneratorBase
       end
     elsif @state == :serialize
       @src += " & #{name}"
+    elsif @state == :to_json
+      _append "data[\"#{name}\"] = #{name};"
+    elsif @state == :from_json
+      if options[:default].nil?
+        _append "if (data[\"#{name}\"].exists())"
+        _append "  #{name} = data[\"#{name}\"].as<#{type}>();"
+      else
+        _append "#{name} = data[\"#{name}\"].defaults_to<#{type}>(#{options[:default]});"
+      end
     end
   end
 
@@ -99,6 +127,11 @@ class FrontGenerator < GeneratorBase
       _append "void set_#{name}_id(#{@id_type} value) { #{name}_id = value; }"
     elsif @state == :serialize
       @src += " & #{name}_id"
+    elsif @state == :to_json
+      _append "data[\"#{name}_id\"] = #{name}_id;"
+    elsif @state == :from_json
+      _append "if (data[\"#{name}_id\"].exists())"
+      _append "  #{name}_id = data[\"#{name}_id\"].as<#{@id_type}>();"
     end
   end
 
@@ -112,6 +145,11 @@ class FrontGenerator < GeneratorBase
       _append "void set_#{singular_name}_ids(const std::vector<#{@id_type}> value) { #{singular_name}_ids = value; }"
     elsif @state == :serialize
       @src += " & #{singular_name}_ids"
+    elsif @state == :to_json
+      _append "data[\"#{singular_name}_ids\"].from_vector(#{singular_name}_ids);"
+    elsif @state == :from_json
+      _append "if (data[\"#{singular_name}_ids\"].is_array())"
+      _append "  #{singular_name}_ids = data[\"#{singular_name}_ids\"].to_vector<#{@id_type}>();"
     end
   end
 
