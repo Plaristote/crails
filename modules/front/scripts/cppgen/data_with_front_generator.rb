87f24bf9596156515e68964c7ee6915038aee466
Michael <michael@unetresgrossebite.com>
2018-12-07 15:19:08 +0100

    * adds cppgen modules DataWithFront and EditWithFront * remove crails-front cppgen generator

---
+++ modules/front/scripts/cppgen/data_with_front_generator.rb
@@ -0,0 +1,69 @@
+$: << (File.dirname(__FILE__) + "/..")
+require 'cppgen/data_generator'
+
+class DataWithFrontGenerator < DataGenerator
+  def generate_class_head object
+    _append_macro "#ifndef #{self.class.client_define}"
+    super object
+    _append_macro "#else"
+    _append "struct #{object[:name]} : public #{self.class.client_super_class}"
+    _append_macro "#endif"
+  end
+
+  def prepare_order_by
+    _append_macro "#ifndef #{self.class.client_define}"
+    super
+    _append_macro "#endif"
+  end
+
+  def edit_resource_declaration
+    with_visibility :protected do
+      _append_macro "#ifdef #{self.class.client_define}"
+      _append "ODB::id_type id = ODB_NULL_ID;"
+      _append_macro "#endif"
+    end
+    _append_macro "#ifdef #{self.class.client_define}"
+    _append "ODB::id_type get_id() const { return id; }"
+    _append "void         set_id(ODB::id_type value) { id = value; }"
+    _append "void         from_json(Data data) { edit(data); }"
+    _append_macro "#endif"
+    super
+  end
+
+  def resource_name name
+    super name
+    _append_macro "#ifdef #{self.class.client_define}"
+    _append "std::string get_resource_name() const { return scope; }"
+    _append_macro "#endif"
+  end
+
+  def has_many_fetch list_type, name, options
+    with_visibility :public do
+      _append_macro "#ifdef #{self.class.client_define}"
+      _append "Crails::Front::Promise fetch_#{name}();"
+      _append_macro "#else"
+      _append "void fetch_#{name}();"
+      _append_macro "#endif"
+    end
+  end
+
+  class << self
+    def client_define
+      "__CHEERP_CLIENT__"
+    end
+
+    def client_super_class
+      "Crails::Front::JsonModel"
+    end
+
+    def generate_includes
+<<CPP
+#{super}
+#ifdef #{client_define}
+# include <crails/front/mvc/model.hpp>
+# include <crails/front/promise.hpp>
+#endif
+CPP
+    end
+  end
+end
---
+++ modules/front/scripts/cppgen/edit_with_front_generator.rb
@@ -0,0 +1,107 @@
+$: << (File.dirname(__FILE__) + "/..")
+require 'cppgen/edit_generator'
+
+class EditWithFrontGenerator < EditGenerator
+  def _append_macro str
+    @src ||= ""
+    @src += str + "\n"
+  end
+
+  def validation type, name, data
+    if data[:uniqueness] == true
+      _append_macro "#ifndef #{DataWithFrontGenerator.client_define}"
+      _append validate_uniqueness type, name
+      _append_macro "#endif"
+      data[:uniqueness] = false
+    end
+    super type, name, data
+  end
+
+  def has_one_getter type, name, options
+    type = get_type type
+    tptr = ptr_type type
+    _append_macro "#ifndef #{DataWithFrontGenerator.client_define}"
+    super
+    _append_macro "#else"
+    _append "#{tptr} #{@klassname}::get_#{name}()"
+    _append "{"
+    _append "  #{tptr} model = std::make_shared<#{type}>();"
+    _append "  model->set_id(get_#{name}_id());"
+    _append "  model->fetch();"
+    _append "  return model;"
+    _append "}"
+    _append_macro "#endif" 
+  end
+
+  def has_many_fetch type, name, options
+    tptr = ptr_type type
+    singular_name = get_singular_name name
+ 
+    _append_macro "#ifndef #{DataWithFrontGenerator.client_define}"
+    super type, name, options
+    _append_macro "#else"
+    _append "Crails::Front::Promise #{@klassname}::fetch_#{name}()"
+    _append "{"
+    @indent += 1
+    _append "std::vector<Crails::Front::Promise> promises;\n"
+    _append "for (auto id : #{singular_name}_ids)"
+    _append "{"
+    @indent += 1
+    _append "#{tptr} model;\n"
+    _append "model->set_id(id);"
+    _append "promises.push_back(model->fetch());"
+    _append "#{name}.push_back(model);"
+    _append "#{name}_fetched = true;"
+    @indent -= 1
+    _append "}"
+    _append "return Crails::Front::Promise::all(promises);"
+    @indent -= 1
+    _append "}"
+    _append_macro "#endif" 
+  end
+
+  def property type, name, options = {}
+    if options[:ready_only] == true && rendering_edit?
+      options[:read_only] = false
+      _append_macro "#ifdef #{DataWithFrontGenerator.client_define}"
+      super
+      _append_macro "#endif"
+    else
+      super
+    end
+  end
+
+  def has_many type, name, options = {}
+    if options[:read_only] == true && rendering_edit?
+      options[:read_only] = false
+      _append_macro "#ifdef #{DataWithFrontGenerator.client_define}"
+      super
+      _append_macro "#endif"
+    else
+      super
+    end
+  end
+
+  def has_one type, name, options = {}
+    if options[:read_only] == true && rendering_edit?
+      options[:read_only] = false
+      _append_macro "#ifdef #{DataWithFrontGenerator.client_define}"
+      super
+      _append_macro "#endif"
+    else
+      super
+    end
+  end
+
+  class << self
+    def generate_includes
+<<CPP
+#ifndef #{DataWithFrontGenerator.client_define}
+#{super}
+#else
+# include <crails/front/mvc/helpers.hpp>
+#endif
+CPP
+    end
+  end
+end
