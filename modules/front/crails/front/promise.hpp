5306a61d9157fd755139839dd78cde4c67ffb7a3
Michael <michael@unetresgrossebite.com>
2018-12-08 00:56:39 +0100

    updated Crails::Front::Promise

--- modules/front/crails/front/promise.hpp
+++ modules/front/crails/front/promise.hpp
@@ -8,7 +8,7 @@ namespace Crails
 {
   namespace Front
   {
-    class Promise : public ObjectImpl<client::Promise<client::Object> >
+    class Promise : public ObjectImpl<client::Promise>
     {
     public:
       static Promise solved_promise;
@@ -16,22 +16,23 @@ namespace Crails
       static Promise all(const std::vector<Promise>& promises);
 
       Promise(std::function<void (std::function<void ()>, std::function<void ()>)> resolver);
-      Promise(client::Promise<client::Object>* ptr) : ObjectImpl(ptr) {}
+      Promise(client::Promise* ptr) : ObjectImpl(ptr) {}
       Promise(client::Object* ptr) : ObjectImpl(ptr) {}
 
       template<typename T>
       Promise then(T callback)
       {
-        client::Object* func = cheerp::Callback(callback);
-        this->apply("then", func);
+        client::EventListener* func = cheerp::Callback(callback);
+        (*this)->then(func);
         return *this;
       }
 
       template<typename T>
       Promise _catch(T callback)
       {
-        client::Object* func = cheerp::Callback(callback);
-        this->apply("catch", func);
+        client::EventListener* func = cheerp::Callback(callback);
+
+	this->apply("catch", Crails::Front::Object(func));
         return *this;
       }
 
@@ -39,26 +40,6 @@ namespace Crails
       void new_promise(client::Object* resolver);
 
       static void prepare_promise_constructor();
-    };
-
-    template<typename RESOLVE_TYPE>
-    class PromiseOf : public Promise
-    {
-    public:
-      PromiseOf(std::function<void (std::function<void (RESOLVE_TYPE)>, std::function<void ()>)> resolver) :
-        Promise(new client::Promise<client::Object>)
-      {
-        client::Object* func = cheerp::Callback([this, resolver](client::Object* _resolve, client::Object* _reject)
-        {
-          this->set("_resolve", _resolve);
-          this->set("_reject",  _reject);
-          resolver(
-            [this](RESOLVE_TYPE value) { this->apply("_resolve", value); },
-            [this]() { this->apply("_reject"); }
-          );
-        });
-        new_promise(func);
-      }
     };
   }
 }
