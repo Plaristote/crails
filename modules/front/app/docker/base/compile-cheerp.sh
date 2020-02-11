9f3e25168115277b0b5d8d69e59114328348662b
Michael <michael@unetresgrossebite.com>
2020-02-11 02:27:26 +0100

    fixed compile-cheerp.sh failing with newer revisions of cheerp

--- modules/front/app/docker/base/compile-cheerp.sh
+++ modules/front/app/docker/base/compile-cheerp.sh
@@ -4,20 +4,21 @@ export CHEERP_SRC=/tmp/compile-cheerp
 
 mkdir -p $CHEERP_SRC
 cd $CHEERP_SRC
-git clone https://github.com/leaningtech/cheerp-llvm
+								# LAST SEEN WORKING WITH TAG:
+git clone https://github.com/leaningtech/cheerp-llvm		# 58650eeadc8b22cf81419ce08ffa1ee7ad21c292
 cd  cheerp-llvm/tools/
-git clone https://github.com/leaningtech/cheerp-clang clang
+git clone https://github.com/leaningtech/cheerp-clang clang	# 224dff3a1c14d2f5697e0ed1565ad019f8430442
 cd $CHEERP_SRC
-git clone https://github.com/leaningtech/cheerp-utils
-git clone https://github.com/leaningtech/cheerp-newlib
-git clone https://github.com/leaningtech/cheerp-libcxx
-git clone https://github.com/leaningtech/cheerp-libcxxabi
-git clone https://github.com/leaningtech/cheerp-libs
+git clone https://github.com/leaningtech/cheerp-utils		# 63b8c37c5faead3483322ba321a3ee4ec7e3498f
+git clone https://github.com/leaningtech/cheerp-newlib		# 875ad4f30b1ade8f005366e76cb66a7dbb411987
+git clone https://github.com/leaningtech/cheerp-libcxx		# 1fadaf39e57b555c699d0deb2277e94f6594651c
+git clone https://github.com/leaningtech/cheerp-libcxxabi	# 9ae50ecf5a7a4ab24113dff37a389cace8b535e1
+git clone https://github.com/leaningtech/cheerp-libs		# 9eb512dd1f6c430dfdf43749dabb554fb6edabc5
 
 cd $CHEERP_SRC/cheerp-llvm
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=/opt/cheerp -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD="ARM;CheerpBackend;CheerpWasmBackend;X86" ..
+cmake -C ../CheerpCmakeConf.cmake -DCMAKE_BUILD_TYPE=Release ..
 make -j4
 make install
 
@@ -30,7 +31,7 @@ make install
 cd $CHEERP_SRC/cheerp-newlib/newlib
 mkdir build
 cd build
-../configure --host=cheerp-genericjs --with-cxx-headers=$CHEERP_SRC/cheerp-libcxx/include --prefix=/opt/cheerp --enable-newlib-io-long-long --enable-newlib-iconv --enable-newlib-iconv-encodings=utf-16,utf-8,ucs_2
+../configure --host=cheerp-genericjs --with-cxx-headers=$CHEERP_SRC/cheerp-libcxx/include --prefix=/opt/cheerp --enable-newlib-io-long-long --enable-newlib-iconv --enable-newlib-iconv-encodings=utf-16,utf-8,ucs_2 --enable-newlib-mb --enable-newlib-nano-formatted-io
 make
 make install
 CHEERP_PREFIX=/opt/cheerp ../build-bc-libs.sh genericjs
@@ -51,4 +52,3 @@ make install
 
 cd $CHEERP_SRC/cheerp-libs
 make -C stdlibs install_genericjs INSTALL_PREFIX=/opt/cheerp CHEERP_PREFIX=/opt/cheerp
-
