e406f955f7eb77ea9a46d84062fdd01ff2fe62d5
Michael <michael@unetresgrossebite.com>
2020-03-29 15:21:54 +0200

    fix metarecord archive_generator generating broken render functions when classname begins with '::'

--- modules/comet/scripts/metarecord/generators/archive_generator.rb
+++ modules/comet/scripts/metarecord/generators/archive_generator.rb
@@ -42,7 +42,7 @@ class ArchiveGenerator < GeneratorBase
   def generate_archive_views object
     @src += "\n"
     ptr_type = "#{object[:classname]}*"
-    funcname_prefix = "render_#{object[:classname].underscore}"
+    funcname_prefix = "render_#{object[:classname].gsub(/^::/,'').underscore}"
     funcname = "#{funcname_prefix}_show_archive"
     _append "#ifndef #{self.class.client_define}"
     _append "std::string #{funcname}(const Crails::Renderer* renderer, Crails::SharedVars& vars)"
