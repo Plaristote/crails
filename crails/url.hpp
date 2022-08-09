f75878e00fcf01f1d0e514f59cbf37b59063cebe
plaristote <michael@unetresgrossebite.com>
2022-08-09 17:15:57 +0200

    implements Crails::Url

---
+++ crails/url.hpp
@@ -0,0 +1,20 @@
+#ifndef  CRAILS_URL_HPP
+# define CRAILS_URL_HPP
+
+# include <string>
+
+namespace Crails
+{
+  struct Url
+  {
+    static Url  from_string(const std::string&);
+    std::string to_string() const;
+
+    bool           ssl = false;
+    std::string    host;
+    unsigned short port = 80;
+    std::string    target;
+  };
+}
+
+#endif
--- modules/proxy/src/rule.cpp
+++ modules/proxy/src/rule.cpp
@@ -3,29 +3,24 @@
 #include <boost/beast/http.hpp>
 #include <boost/asio.hpp>
 #include <boost/lexical_cast.hpp>
-#include "crails/logger.hpp"
+#include "crails/url.hpp"
 
 using namespace std;
 using namespace Crails;
 
-ProxyRequestHandler::ProxyRequestHandler::Rule::Rule(const char* regex, const char* url, ProxyRequestHandler::Mode mode) :
+ProxyRequestHandler::ProxyRequestHandler::Rule::Rule(const char* regex, const char* url_string, ProxyRequestHandler::Mode mode) :
   matcher(regex, std::regex_constants::optimize),
   mode(mode)
 {
-  static const std::regex url_matcher("^(https?)://([^/:]+)(:([0-9]{1,5}))?/?(.*)$");
-  std::smatch matches;
+  Url url = Url::from_string(url_string);
   ProxyRequest base;
-  std::string a(url);
 
-  if (std::regex_match(a, matches, url_matcher))
-  {
-    base.method(boost::beast::http::verb::get);
-    base.ssl    = matches[1].str() == "https";
-    base.host   = matches[2].str();
-    base.port   = matches[4].str().length() > 0 ? boost::lexical_cast<unsigned short>(matches[4].str()) : (base.ssl ? 443 : 80);
-    base.target(matches[5].str());
-    solver = std::bind(&Rule::defaultSolver, base, placeholders::_1, placeholders::_2);
-  }
+  base.method(boost::beast::http::verb::get);
+  base.ssl  = url.ssl;
+  base.host = url.host;
+  base.port = url.port;
+  base.target(url.target);
+  solver = std::bind(&Rule::defaultSolver, base, placeholders::_1, placeholders::_2);
 }
 
 ProxyRequestHandler::Rule::Rule(const char* regex, ProxyRequestHandler::RuleSolver solver) : matcher(regex), mode(ProxyRequestHandler::Proxy), solver(solver)
